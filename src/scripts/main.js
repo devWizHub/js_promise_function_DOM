3a9b9be158f55d46e8dede44c2d9aa05836197b9
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-01-22 18:43:52 +0100

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
-function waitFor(element, eventName) {
-// write your code here
+async function waitFor(element, eventName) {
+  // write your code here
+  const loginField = document.getElementById('login');
+  // const passwordField = document.getElementById('password');
+  // const button = document.getElementById('submit');
+
+  const resolver = (complete, cancel) => {
+    loginField.addEventListener('login', () => {
+      complete('');
+    });
+  };
+
+  return new Promise(resolver);
 }
 
 const printMessage = (message) => {
-// write your code here
+  // write your code here
 };
 
 module.exports = {
